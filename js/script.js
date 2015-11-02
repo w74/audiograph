32b84c498abdc0e6fdeb5241a2937002a7291fba
Wolfram Rong <wolfram.rong@gmail.com>
2015-11-01 19:45:25 -0600

    wave + string
    added wave and string visualizations

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ function init(){
 	//setTimeout(function(){document.getElementsByTagName("header")[0].style.display="none";}, 3500);
 	loadVars();
 	changeAnim(activeIndex);
-};
+}
 
 /*FUNCTION loadVars() 
 		desc: sets up the audio object (=a) and the canvas object (=c)
@@ -71,6 +71,14 @@ function changeAnim(){
 			num = 16;
 			a.analyser.fftSize = 64;
 			break;
+		case 3: //wave
+			num = ~~(c.width/100);
+			a.analyser.fftSize = 128;
+			break;
+		case 4: //string
+			num = ~~(c.width/50);
+			a.analyser.fftSize = 128;
+			break;
 	}
 	setUpCanvas(num);
 }
@@ -104,11 +112,12 @@ function setUpCanvas(num){
 	switch(activeIndex){
 		case 0:
 			var barWidth = ~~(c.width/num);
+
 			for(var i = 0; i < num; i++){
 				var pnt = new Point(i*barWidth, c.height*0.995);
 				var siz = new Size(barWidth-10, 3);
 				var shp = new Shape.Rectangle({
-					point: pnt, 
+					point: pnt,
 					size: siz,
 					fillColor: 'black'
 				});
@@ -132,14 +141,33 @@ function setUpCanvas(num){
 				strokeColor: 'black',
 				strokeWidth: 10,
 				closed: true
-			})
+			});
 			var segmentArray = [];
+
 			for(var i = 0; i < num; i++){
 				var hypotneuse = (i%2 === 0 ? 60 : 50);
 				segmentArray[i] = new Point(view.center.x - hypotneuse * Math.cos(i*0.39269915) , view.center.y - hypotneuse * Math.sin(i*0.39269915));
 			}
 			path.addSegments(segmentArray);
 			project.activeLayer.addChild(path.smooth());
+			break;
+		case 3:
+		case 4:
+			var path = new Path({
+				strokeColor: 'black',
+				strokeWidth: 10,
+				strokeCap: 'round'
+			});
+			var segmentArray = [[0, view.center.y]];
+
+			var gapWidth = ~~((c.width-20)/num);
+			for(var i = 1; i < num; i++){
+				segmentArray[i] = new Point((gapWidth*i), view.center.y + Math.pow(-1, i) * 20);
+			}
+			segmentArray.push([c.width, view.center.y])
+			path.addSegments(segmentArray);
+			project.activeLayer.addChild(path.smooth());
+			break;
 	}
 }
 
@@ -184,10 +212,34 @@ function onFrame(e){
 						Math.log(x+1)/log(255) -> significantly raises the amplitude of x[i] when below 100 to decrease the liklihood of a large arc
 						meanFeed/550 -> grows and shrinks entire blob; range approx. [0, 0.45)
 						30 -> minimum hypotneuse length of prevent point from falling into center of view */
-				layer[0].segments[i].point.x = view.center.x - hypotneuse * Math.cos(i * 0.39269915 + e.count/300);
-				layer[0].segments[i].point.y = view.center.y - hypotneuse * Math.sin(i * 0.39269915 + e.count/300);
+				layer[0].segments[i].point.x = view.center.x - hypotneuse * Math.cos(i * 0.39269915 + e.count/400);
+				layer[0].segments[i].point.y = view.center.y - hypotneuse * Math.sin(i * 0.39269915 + e.count/400);
 				/*	22.5deg -> 0.39269915rad
-						e.count/300 -> rotates blob by ~0.2 degrees per frame */
+						e.count/300 -> rotates blob by ~0.14 degrees per frame */
+			}
+			layer[0].smooth();
+			break;
+		case 3:
+			updateFeed(layer[0].segments.length-1, 9);
+			for(var i = 1; i < layer[0].segments.length-1; i++){
+				layer[0].segments[i].point.y = view.center.y - (Math.pow(-1, i) * ((Math.sin(a.feed[i]/20)+1)/15 * c.height + 2));
+				/* Equation Breakdown:
+						view.center.y -> vertically aligns all points
+						Math.pow(-1, i) -> alternates between high and low points
+						(Math.sin(x/20)+1) -> sin(x/20) used to make wave patterns more unpredictable, but the period increased 20x to preserve a visual pattern; we add 1 to make the range [0, 2]
+						Math.sin(...)/15 -> limits range to (.366, .634) of view height
+						... + 2 -> prevents flatlining of wave*/
+			}
+			layer[0].smooth();
+			break;
+		case 4:
+			updateFeed(layer[0].segments.length-1, 5);
+			for(var i = 1; i < layer[0].segments.length-1; i++){
+				layer[0].segments[i].point.y = view.center.y - ((Math.sin(a.feed[i]/20)/10 * c.height));
+				/* Equation Breakdown:
+						view.center.y -> vertically aligns all points
+						Math.sin(x/20) -> sin(x/20) used to make wave patterns more unpredictable, but the period increased 20x to preserve a visual pattern
+						Math.sin(...)/10 -> limits range to [.4, .6] of view height; a larger range creates unpleasant artifacts on monitors with low refresh rates*/
 			}
 			layer[0].smooth();
 			break;
